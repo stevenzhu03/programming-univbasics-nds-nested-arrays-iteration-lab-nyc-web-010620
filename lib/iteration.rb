6cdb518ede19d29b68d944c515a52a88b20e4359
Steven Zhu <stevenzhu03@gmail.com>
2019-12-28 21:10:31 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -10,7 +10,7 @@ def join_ingredients(src)
   result = []
   
   i=0
-  while i < scr.length do
+  while i < src.length do
     inner = src[i]
     result << "I love #{inner[0]} and #{inner[1]} on my pizza"
     i += 1
@@ -27,7 +27,7 @@ def find_greater_pair(src)
   result = []
   
   i=0
-  while i < scr.length do
+  while i < src.length do
     result << (scr[i][0] > scr[i][1] ? scr[i][0] : scr[i][1])
     i +=1
 end
