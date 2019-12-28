6d2c1e1498a5d4b8490fbb92334bb525d8d08831
Steven Zhu <stevenzhu03@gmail.com>
2019-12-28 21:10:59 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -28,7 +28,7 @@ def find_greater_pair(src)
   
   i=0
   while i < src.length do
-    result << (scr[i][0] > scr[i][1] ? scr[i][0] : scr[i][1])
+    result << (src[i][0] > src[i][1] ? src[i][0] : src[i][1])
     i +=1
 end
 
